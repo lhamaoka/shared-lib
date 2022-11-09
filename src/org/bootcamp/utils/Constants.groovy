763bec3c98f0b993a248690d5618ec93e0ba3e7c
lhamaoka <117431954+lhamaoka@users.noreply.github.com>
2022-11-09 16:48:32 +0100

    Add files via upload

---
+++ src/org/bootcamp/utils/Constants.groovy
@@ -0,0 +1,9 @@
+package org.bootcamp.utils
+
+class Constants {
+    //labals for environment
+    static final ENVIRONMENT_DEV = 'dev'
+    static final ENVIRONMENT_INT = 'int'
+    static final ENVIRONMENT_PRE = 'pre'
+    static final ENVIRONMENT_PRO = 'pro'
+}
---
+++ vars/getPomVersion.groovy
@@ -0,0 +1,3 @@
+def call(){
+    println('Getting POM Version');
+}
